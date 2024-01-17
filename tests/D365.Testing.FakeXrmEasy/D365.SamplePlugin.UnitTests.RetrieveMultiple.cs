c5fb42e62171d77fe13856e2bb316c85add018d4
alyousse <alyousse@microsoft.com>
2024-01-17 07:01:23 -0600

    Added RM example

---
+++ tests/D365.Testing.FakeXrmEasy/D365.SamplePlugin.UnitTests.RetrieveMultiple.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using D365.Testing.FakeXrmEasy.TestConfig;
+using FakeXrmEasy;
+using FakeXrmEasy.Plugins;
+using Microsoft.Crm.Sdk.Messages;
+using Microsoft.Identity.Client;
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.Xrm.Sdk;
+
+namespace D365.Testing{
+    [TestClass]
+    public class D365SamplePluginUnitTestsRetrieveMultiple : FakeXrmEasyTestsBase
+    {
+
+        public string BuildUnsecureConfig()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append("<config><settings>");
+            sb.Append("<setting>");
+            sb.Append(String.Format("<name={0}>", ""));
+            sb.Append(String.Format("<order={0}>", ""));
+            sb.Append(String.Format("<type={0}>", ""));
+            sb.Append("</setting>");
+            sb.Append("</settings></config>");
+
+            return sb.ToString();
+        }
+
+
+        [TestMethod]
+        public void TestFakedRetrieveMultiple()
+        {
+
+            //Arrange
+            var pluginContext = _context.GetDefaultPluginContext();
+
+            //XrmFakedContext context = new XrmFakedContext();
+            //IOrganizationService service = context.GetOrganizationService();
+
+            D365.SamplePlugin.RetrieveMultiplePlugin plugin = new SamplePlugin.RetrieveMultiplePlugin();
+
+            pluginContext.MessageName = "RetrieveMultiple";
+            pluginContext.Stage = 40;
+
+            string unsecureConfig = "";
+            string secureConfig = "";
+
+            List<Entity> initialEntities = new List<Entity>();
+            int excessNumberOfRecords = 50;
+            //_context.MaxRetrieveCount = 1000;
+            //_context.
+            for (int i = 0; i < 1000 + excessNumberOfRecords; i++)
+            {
+                Entity e = new Entity("account");
+                e.Id = Guid.NewGuid();
+                initialEntities.Add(e);
+            }
+            _context.Initialize(initialEntities);
+
+            try
+            {
+                _context.ExecutePluginWith(pluginContext, plugin);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
---
+++ tests/D365.Testing.SamplePlugin/RetrieveMultiplePlugin.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D365.SamplePlugin
+{
+    public class RetrieveMultiplePlugin : IPlugin
+    {
+
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            // Obtain the execution context
+            IPluginExecutionContext context = (IPluginExecutionContext)
+              serviceProvider.GetService(typeof(IPluginExecutionContext));
+
+            // Obtain the IOrganizationService instance 
+            IOrganizationServiceFactory serviceFactory =
+              (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
+            IOrganizationService orgService = serviceFactory.CreateOrganizationService(context.UserId);
+
+            // Obtain the Tracing service reference
+            ITracingService tracingService =
+              (ITracingService)serviceProvider.GetService(typeof(ITracingService));
+            QueryExpression query = new QueryExpression("account");
+            EntityCollection result = orgService.RetrieveMultiple(query);
+            // Verify input parameters
+
+        }
+    }
+}
